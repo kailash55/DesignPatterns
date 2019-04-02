5152d6a0016f693b71dfd06a1e7109970342312c
kailash55 <kailashdesiti5@gmail.com>
2019-04-03 00:55:55 +0530

    Initial changes

---
+++ AspPatterns.Chap2/AspPatterns.Chap2.Service/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace AspPatterns.Chap2.Service
+{
+    public interface IProductRepository
+    {
+        IList<Product> GetAllProductsIn(int categoryId);
+    }
+}
---
+++ AspPatterns.Chap2/AspPatterns.Chap2.Service/ProductService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AspPatterns.Chap2.Service
+{
+    public class ProductService
+    {
+        private IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public IList<Product> GetAllProductsIn(int categoryId)
+        {
+            IList<Product> products;
+            string storageKey = string.Format("products_in_category_id_{0}", categoryId);
+
+            products = GetListFromCache();
+
+            if(products == null)
+            {
+                products = _productRepository.GetAllProductsIn(categoryId);
+                InsertIntoCache(storageKey, products);
+            }
+
+            return products;
+        }
+
+        private void InsertIntoCache(string storageKey, IList<Product> products)
+        {
+            //Code to insert product List into cache
+            throw new NotImplementedException();
+        }
+
+        private IList<Product> GetListFromCache()
+        {
+            //Code to get Product List from cache
+            throw new NotImplementedException();
+        }
+    }
+}
