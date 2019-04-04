3f9c50a58e799a166ec28b3a0ece2ceb57ed3e3d
kailash55 <kailashdesiti5@gmail.com>
2019-04-04 23:21:45 +0530

    Implemented Null Object Pattern by adding NullCacheAdapter which implements ICacheStorage interface and does nothing on execution

---
+++ AspPatterns.Chap2/AspPatterns.Chap2.Service/NullCacheAdapter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspPatterns.Chap2.Service
+{
+    public class NullCacheAdapter : ICacheStorage
+    {
+        public void Remove(string key)
+        {
+            //Do nothing
+        }
+
+        public T Retreive<T>(string key)
+        {
+            return default(T);
+        }
+
+        public void Store(string key, object data)
+        {
+            //Do nothing
+        }
+    }
+}
