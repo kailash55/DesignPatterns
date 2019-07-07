0213deb41dbe94ba3d6313a38cbf07255eaad03a
kailash55 <kailashdesiti5@gmail.com>
2019-07-08 00:32:10 +0530

    Added EventTicket Solution

---
+++ ASPPatterns.Chap6.EventTickets/ASPPatterns.Chap6.EventTickets.Model/Event.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASPPatterns.Chap6.EventTickets.Model
+{
+    public class Event
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Allocation { get; set; }
+        public List<TicketReservation> ReservedTickets { get; set; }
+        public List<TicketPurchase> PurchasedTickets { get; set; }
+
+        public Event()
+        {
+            ReservedTickets = new List<TicketReservation>();
+            PurchasedTickets = new List<TicketPurchase>();
+        }
+
+        public int AvailableAllocation()
+        {
+            int salesAndReservation = 0;
+
+            PurchasedTickets.ForEach(t => salesAndReservation += t.TicketQuantity);
+            ReservedTickets.FindAll(r => r.IsStillAlive())
+                .ForEach(r => salesAndReservation += r.TicketQuantity);
+
+            return Allocation - salesAndReservation;
+        }
+
+        public bool CanPurchaseTicketWith(Guid reservationId)
+        {
+            if (HasReservationWith(reservationId))
+                return GetReservationWith(reservationId).IsStillAlive();
+
+            return false;
+        }
+
+        private TicketReservation GetReservationWith(Guid reservationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool HasReservationWith(Guid reservationId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ASPPatterns.Chap6.EventTickets/ASPPatterns.Chap6.EventTickets.Model/TicketReservationFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASPPatterns.Chap6.EventTickets.Model
+{
+    public class TicketReservationFactory
+    {
+        public static TicketReservation CreateReservation(Event Event, int tktQty)
+        {
+            TicketReservation ticketReservation = new TicketReservation();
+
+            ticketReservation.Id = Guid.NewGuid();
+            ticketReservation.Event = Event;
+            ticketReservation.ExpiryTime = DateTime.Now.AddMinutes(1);
+            ticketReservation.TicketQuantity = tktQty;
+
+            return ticketReservation;
+        }
+    }
+}
